8e3d0c013f31763a9f2ae15a8d1517399a34cf49
Artur Juraszek <artur@juraszek.xyz>
2019-06-12 16:29:56 +0200

    Propose a schema for the database

--- main/main.go
+++ main/main.go
@@ -1,9 +1,15 @@
 package main
 
-import "bufio"
-import "encoding/json"
-import "fmt"
-import "os"
+import (
+	"bufio"
+	"database/sql"
+	"fmt"
+	"os"
+
+	_ "github.com/lib/pq"
+	"github.com/tidwall/gjson"
+	// "github.com/asgavar/partia"
+)
 
 // import "github.com/asgavar/partia"
 
@@ -23,11 +29,39 @@ func main() {
 		lines = append(lines, scanner.Text())
 	}
 
-	fmt.Println(isItFirstRun)
+	dbName := gjson.Get(lines[0], "open.database").String()
+	dbLogin := gjson.Get(lines[0], "open.login").String()
+	dbPassword := gjson.Get(lines[0], "open.password").String()
 
-	for _, commandInvocation := range lines[1:] {
-		var buf map[string]interface{}
-		json.Unmarshal([]byte(commandInvocation), &buf)
-		fmt.Println(buf)
+	if isItFirstRun {
+		createDbAndTables(dbName, dbLogin, dbPassword)
 	}
+
+	dbUri := fmt.Sprintf(
+		"postgres://%s:%s@localhost/%s?sslmode=disable", dbLogin, dbPassword, dbName)
+
+	db, err := sql.Open("postgres", dbUri)
+	fmt.Println(err)
+	fmt.Println(db)
+
+	// 	for _, commandInvocation := range lines[1:] {
+	// 	}
+}
+
+func createDbAndTables(db, login, password string) {
+	dbAsInit, _ := sql.Open("postgres", "postgres://init:qwerty@localhost?sslmode=disable")
+
+	createRoleSql := fmt.Sprintf("CREATE ROLE %s LOGIN PASSWORD '%s'", login, password)
+	createDbSql := fmt.Sprintf("CREATE DATABASE %s", db)
+	grantConnectSql := fmt.Sprintf("GRANT CONNECT ON DATABASE %s TO %s", db, login)
+
+	dbAsInit.Query(createRoleSql)
+	dbAsInit.Query(createDbSql)
+	dbAsInit.Query(grantConnectSql)
+
+	// TODO create actual tables
+}
+
+func openDbConnection() {
+
 }
---
+++ setup-db.sql
@@ -0,0 +1,85 @@
+CREATE TABLE member (
+  id INTEGER PRIMARY KEY,
+  password VARCHAR
+);
+
+CREATE TABLE member_isleader (
+  member_id INTEGER REFERENCES member(id)
+);
+
+CREATE TABLE member_lastactive (
+  member_id INTEGER REFERENCES member(id),
+  last_active TIMESTAMP
+);
+
+CREATE TABLE authority (
+  id INTEGER PRIMARY KEY
+);
+
+CREATE TABLE project (
+  id INTEGER PRIMARY KEY,
+  authority INTEGER REFERENCES authority(id)
+);
+
+CREATE TYPE action_type AS ENUM ('support', 'protest');
+
+CREATE TABLE action (
+  id INTEGER PRIMARY KEY,
+  proposed_by INTEGER REFERENCES member(id),
+  project_id INTEGER REFERENCES project(id),
+  of_type action_type
+);
+
+CREATE TABLE already_used_numbers (
+  number INTEGER PRIMARY KEY
+);
+
+CREATE TABLE vote (
+  timestamp TIMESTAMP,
+  member_id INTEGER REFERENCES member(id),
+  action_id INTEGER REFERENCES action(id)
+);
+
+CREATE TABLE upvote () INHERITS(vote);
+CREATE TABLE downvote () INHERITS(vote);
+
+CREATE TABLE trolltracker (
+  member_id INTEGER REFERENCES member(id),
+  saldo INTEGER
+);
+
+CREATE OR REPLACE FUNCTION increment_trolltracker() RETURNS trigger AS $xD$
+  BEGIN
+    UPDATE trolltracker SET saldo = saldo + 1 WHERE member_id = NEW.member_id;
+    RETURN NEW;
+  END
+$xD$ LANGUAGE plpgsql;
+
+CREATE TRIGGER upvote_trolltracker
+  AFTER INSERT ON upvote
+  FOR EACH ROW
+    EXECUTE PROCEDURE increment_trolltracker();
+
+CREATE OR REPLACE FUNCTION decrement_trolltracker() RETURNS trigger AS $xD$
+BEGIN
+  UPDATE trolltracker SET saldo = saldo - 1 WHERE member_id = NEW.member_id;
+  RETURN NEW;
+END
+$xD$ LANGUAGE plpgsql;
+
+CREATE TRIGGER downvote_trolltracker
+  AFTER INSERT ON downvote
+  FOR EACH ROW
+    EXECUTE PROCEDURE decrement_trolltracker();
+
+CREATE OR REPLACE FUNCTION create_a_trolltracker_entry() RETURNS trigger AS $xD$
+BEGIN
+  INSERT INTO trolltracker VALUES (NEW.id, 0);
+  RETURN NEW;
+END
+$xD$ LANGUAGE plpgsql;
+
+CREATE TRIGGER member_create_a_trolltracker_entry
+  AFTER INSERT ON member
+  FOR EACH ROW
+    EXECUTE PROCEDURE create_a_trolltracker_entry();
